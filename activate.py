75e3d3832fbd8b17f6deeb5c518e38fe68c5985c
swathi <swathireddymarella70@gmail.com>
2019-07-26 11:28:24 +0530

    batch file added

---
+++ activate.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+
+os.system("pip install -r C:\Users\marella.swathi\Desktop\\requirements.txt")
+executable_file="python "+sys.argv[1]
+os.system(executable_file)
---
+++ batch.bat
@@ -0,0 +1,2 @@
+@echo off
+cmd /k "cd /d C:\Users\marella.swathi\Desktop\myenv\Scripts & activate & cd /d    C:\Users\marella.swathi\Desktop & python activate.py hello.py & deactivate"
